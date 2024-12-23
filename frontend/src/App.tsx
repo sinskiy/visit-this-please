3d1b092601ccd594bf8fd5c43dc1e4383c48a120
fluqont <fluqont@gmail.com>
2024-12-23 23:03:55 +0300

    feat: set up basic communication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+import env from "./env";
+
 export default function App() {
-  return <></>;
+  const [data, setData] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(env.VITE_API_URL);
+      const json = await response.json();
+      setData(json);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+
+  return <>{data?.hello}</>;
 }
