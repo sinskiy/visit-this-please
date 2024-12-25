67aa68ac449b95b7e99ef6539eda755b81741494
fluqont <fluqont@gmail.com>
2024-12-25 17:58:37 +0300

    frontend: add react query and use for fake sign up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,65 @@
-import { useEffect, useState } from "react";
-import env from "./env";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useMutation,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { mutateApi } from "./lib/fetch";
+import { handleSubmit } from "./lib/events";
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 1000 * 60 * 5 } },
+});
 
 export default function App() {
-  const [data, setData] = useState<undefined | { hello: string }>();
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(env.VITE_API_URL);
-      const json = await response.json();
-      setData(json);
-    }
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SignUp />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
 
-    fetchData();
-  }, []);
+function SignUp() {
+  const { isPending, isError, error, mutate } = useMutation({
+    mutationFn: ({
+      username,
+      password,
+    }: {
+      username: string;
+      password: string;
+    }) => mutateApi("POST", "/sign-up", { username, password }),
+  });
 
-  return <>{data?.hello}</>;
+  if (isError) {
+    console.error(error);
+  }
+
+  function signUpAction(formData: FormData) {
+    mutate({
+      username: formData.get("username")! as string,
+      password: formData.get("password")! as string,
+    });
+  }
+
+  return (
+    <form onSubmit={(e) => signUpAction(handleSubmit(e))}>
+      <input
+        type="text"
+        name="username"
+        id="username"
+        autoComplete="username"
+      />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        autoComplete="new-password"
+      />
+      <button type="submit" disabled={isPending}>
+        submit
+      </button>
+      {isError && error.message}
+    </form>
+  );
 }
---
+++ frontend/src/lib/fetch.ts
@@ -0,0 +1,34 @@
+import env from "../env";
+
+export async function queryApi(path: string, options: RequestInit) {
+  const response = await fetch(env.VITE_API_URL + path, options);
+
+  // json call may fail if server is shut down, for example
+  try {
+    const json = await response.json();
+    if (!response.ok) {
+      throw new Error(json);
+    } else {
+      return json;
+    }
+  } catch {
+    throw new Error(response.statusText);
+  }
+}
+
+// put is dangerous and i prefer not to use it, don't add for now
+type HttpMutateMethod = "POST" | "PATCH";
+
+export async function mutateApi(
+  method: HttpMutateMethod,
+  path: string,
+  body: object,
+  options?: RequestInit
+) {
+  return queryApi(path, {
+    ...options,
+    method,
+    headers: { ...options?.headers, "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+}
