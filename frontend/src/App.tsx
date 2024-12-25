b95b8e783a549ec9e3fd9f5083af7c527197e458
sinskiy <sinskiy@proton.me>
2024-12-25 18:26:00 +0300

    feat(front): validate with zod and react-hook-form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,9 @@ import {
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { mutateApi } from "./lib/fetch";
-import { handleSubmit } from "./lib/events";
+import { infer as inferType, object, string } from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: 1000 * 60 * 5 } },
@@ -20,6 +22,17 @@ export default function App() {
   );
 }
 
+const signUpSchema = object({
+  username: string().min(1).max(100),
+  password: string().min(1).max(100),
+  confirmPassword: string().min(1).max(100),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords do not match",
+  path: ["confirmPassword"],
+});
+
+type SignUpSchema = inferType<typeof signUpSchema>;
+
 function SignUp() {
   const { isPending, isError, error, mutate } = useMutation({
     mutationFn: ({
@@ -31,35 +44,43 @@ function SignUp() {
     }) => mutateApi("POST", "/sign-up", { username, password }),
   });
 
-  if (isError) {
-    console.error(error);
-  }
+  const onSignUp: SubmitHandler<SignUpSchema> = ({ username, password }) => {
+    mutate({ username, password });
+  };
 
-  function signUpAction(formData: FormData) {
-    mutate({
-      username: formData.get("username")! as string,
-      password: formData.get("password")! as string,
-    });
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpSchema>({ resolver: zodResolver(signUpSchema) });
 
   return (
-    <form onSubmit={(e) => signUpAction(handleSubmit(e))}>
+    <form onSubmit={handleSubmit(onSignUp)}>
       <input
         type="text"
-        name="username"
         id="username"
         autoComplete="username"
+        {...register("username")}
       />
+      <p>{errors.username?.message}</p>
       <input
         type="password"
-        name="password"
         id="password"
         autoComplete="new-password"
+        {...register("password")}
       />
+      <p>{errors.password?.message}</p>
+      <input
+        type="password"
+        id="confirm-password"
+        autoComplete="new-password"
+        {...register("confirmPassword")}
+      />
+      <p>{errors.confirmPassword?.message}</p>
       <button type="submit" disabled={isPending}>
         submit
       </button>
-      {isError && error.message}
+      <p>{isError && error.message}</p>
     </form>
   );
 }
