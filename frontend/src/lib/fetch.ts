7fc855ec1e0f406f22f24c16ae7a454a7c150149
sinskiy <sinskiy@proton.me>
2024-12-25 19:26:12 +0300

    feat(frontend): improve fetch wrapper

--- frontend/src/lib/fetch.ts
+++ frontend/src/lib/fetch.ts
@@ -3,17 +3,21 @@ import env from "./env";
 export async function queryApi(path: string, options: RequestInit) {
   const response = await fetch(env.VITE_API_URL + path, options);
 
-  // json call may fail if server is shut down, for example
-  try {
-    const json = await response.json();
-    if (!response.ok) {
-      throw new Error(json);
-    } else {
-      return json;
+  if (!response.ok) {
+    if (response.headers.get("Content-Type") !== "application/json") {
+      throw new Error(response.statusText);
     }
-  } catch {
-    throw new Error(response.statusText);
+
+    const json = await response.json();
+    if (!("error" in json)) {
+      throw new Error(response.statusText);
+    }
+
+    throw new Error(json.error);
   }
+
+  const json = await response.json();
+  return json;
 }
 
 // put is dangerous and i prefer not to use it, don't add for now
