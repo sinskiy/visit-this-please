317d1971d2a9a9b60c96eabf7c8b0f85965fc27f
sinskiy <sinskiy@proton.me>
2024-12-28 14:10:08 +0300

    chore(db): update pool

--- backend/src/db/pool.ts
+++ backend/src/db/pool.ts
@@ -1,7 +1,8 @@
-import { User } from "./config.ts";
+import { Place, User } from "./config.ts";
 
 try {
   await User.deleteMany();
+  await Place.deleteMany();
 } catch (e) {
   console.error("❌ Mongoose error pooling:", e);
   process.exit(1);
