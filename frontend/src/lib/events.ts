4b0e417d62bb255d33e05224841ffd0788d22e57
sinskiy <sinskiy@proton.me>
2024-12-28 11:29:20 +0300

    chore(frontend): remove unused events.ts

--- frontend/src/lib/events.ts
+++
@@ -1,8 +0,0 @@
-import { FormEvent } from "react";
-
-// onSubmit={(e) => exampleAction(handleSubmit(e))}
-export function handleSubmit(e: FormEvent<HTMLFormElement>) {
-  e.preventDefault();
-  const formData = new FormData(e.currentTarget);
-  return formData;
-}
