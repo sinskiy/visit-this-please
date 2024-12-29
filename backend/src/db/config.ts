eb36e19a74de3f3db373d1267f953de0b284b4e1
sinskiy <sinskiy@proton.me>
2024-12-29 10:56:41 +0300

    feat(backend): allow only existing countries

--- backend/src/db/config.ts
+++ backend/src/db/config.ts
@@ -1,5 +1,6 @@
 import { Schema, connect, model } from "mongoose";
 import env from "../lib/env.ts";
+import { COUNTRIES } from "../lib/const.ts";
 
 const userSchema = new Schema({
   username: { type: String, unique: true, required: true },
@@ -20,7 +21,7 @@ const voteSchema = new Schema({
 const placeSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
 
-  country: { type: String, required: true },
+  country: { type: String, enum: COUNTRIES, required: true },
   stateOrRegion: String,
   settlement: String,
   name: String,
