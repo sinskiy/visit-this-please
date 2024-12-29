9d5fee575a1c49ad2dff77fb5f3d8757a8620557
fluqont <fluqont@gmail.com>
2024-12-29 12:35:38 +0300

    refactor(frontend): move input with select out

---
+++ frontend/src/ui/InputFieldWithSelect.tsx
@@ -0,0 +1,75 @@
+import { ChangeEvent, useMemo, useState } from "react";
+import InputField from "./InputField";
+import { FieldError, UseFormRegister, UseFormSetValue } from "react-hook-form";
+import { AddPlaceSchema } from "../pages/Home";
+
+interface InputFieldWithSelectProps {
+  id: keyof AddPlaceSchema;
+  values: readonly string[];
+  isSelected: boolean;
+  setIsSelected: (state: boolean) => void;
+  register: UseFormRegister<AddPlaceSchema>;
+  error: FieldError | undefined;
+  setValue: UseFormSetValue<AddPlaceSchema>;
+}
+
+let timeout: NodeJS.Timeout;
+export default function InputFieldWithSelect({
+  id,
+  values,
+  isSelected,
+  setIsSelected,
+  error,
+  setValue,
+  register,
+}: InputFieldWithSelectProps) {
+  const [search, setSearch] = useState("");
+
+  const result = useMemo(
+    () =>
+      values.filter((country) =>
+        country.toLowerCase().includes(search.toLowerCase() ?? "")
+      ),
+    [search]
+  );
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    if (isSelected) {
+      setIsSelected(false);
+    }
+
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      setSearch(e.target.value);
+    }, 1000);
+  }
+
+  return (
+    <>
+      <InputField
+        id={id}
+        type="text"
+        error={error}
+        {...register(id, { onChange: handleChange })}
+      />
+      {search && (
+        <ul>
+          {result.map((value) => (
+            <li key={value}>
+              <button
+                onClick={() => {
+                  setValue(id, value);
+                  setSearch(value);
+                  setIsSelected(true);
+                }}
+                type="button"
+              >
+                {value}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
