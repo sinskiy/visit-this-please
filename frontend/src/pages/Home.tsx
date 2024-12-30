5d2cda19ce61a0a406ef78d3bc4aa97ba19652cf
fluqont <fluqont@gmail.com>
2024-12-30 10:43:24 +0300

    refactor(frontend): Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -15,15 +15,27 @@ export default function Home() {
   const page = Number(params.get("page") ?? 1);
   const search = params.get("search") ?? "";
 
+  function setSearch(search?: string) {
+    params.set("search", search ?? "");
+    setParams(params);
+  }
+  function setSort(type: string) {
+    params.set("sort", type);
+    params.set("page", "1");
+    setParams(params);
+  }
+  function setPrevPage() {
+    params.set("page", String(page - 1));
+    setParams(params);
+  }
+  function setNextPage() {
+    params.set("page", String(page + 1));
+    setParams(params);
+  }
+
   const dialogRef = useRef<HTMLDialogElement>(null);
 
-  const { data, isLoading, isError, error } = useQuery<Place[]>({
-    queryKey: ["places", sort, page, search],
-    queryFn: () =>
-      queryApi(`/places?sort=${sort}&page=${page}&search=${search}`, {
-        credentials: "include",
-      }),
-  });
+  const { data, isLoading, isError, error } = usePlaces({ sort, page, search });
 
   const {
     isPending: isVoteLoading,
@@ -31,8 +43,6 @@ export default function Home() {
     error: voteError,
     mutate,
   } = useVote();
-
-  const searchRef = useRef<HTMLInputElement>(null);
 
   return (
     <>
@@ -43,34 +53,8 @@ export default function Home() {
           add place
         </button>
       )}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          params.set("search", searchRef.current?.value ?? "");
-          setParams(params);
-        }}
-      >
-        <input type="search" name="search" id="search" ref={searchRef} />
-        <button type="submit">submit</button>
-      </form>
-      <select
-        name="sort"
-        id="sort"
-        defaultValue={sort}
-        onChange={(e) => {
-          params.set("sort", e.currentTarget.value);
-          params.set("page", "1");
-          setParams(params);
-        }}
-      >
-        <option value="upvotes">upvotes</option>
-        <option value="downvotes">downvotes</option>
-        <option value="votes">votes</option>
-        <option value="positivity">positivity</option>
-        <option value="negativity">negativity</option>
-        <option value="last-added">last added</option>
-        <option value="last-voted">last voted</option>
-      </select>
+      <Search setSearch={setSearch} />
+      <Sort sort={sort} setSort={setSort} />
       {/* TODO: better loading (skeleton) */}
       {isLoading === true ? (
         <p>loading...</p>
@@ -92,24 +76,11 @@ export default function Home() {
       ) : (
         <p>no places</p>
       )}
-      <button
-        onClick={() => {
-          params.set("page", String(page - 1));
-          setParams(params);
-        }}
-        disabled={page <= 1}
-      >
-        prev
-      </button>
-      {/* TODO: max page */}
-      <button
-        onClick={() => {
-          params.set("page", String(page + 1));
-          setParams(params);
-        }}
-      >
-        next
-      </button>
+      <Pagination
+        page={page}
+        setPrevPage={setPrevPage}
+        setNextPage={setNextPage}
+      />
       {isVoteLoading ? (
         <p>voting...</p>
       ) : (
@@ -175,4 +146,86 @@ function useVote() {
   });
 
   return mutation;
+}
+
+function Search({ setSearch }: { setSearch: (search?: string) => void }) {
+  const searchRef = useRef<HTMLInputElement>(null);
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setSearch(searchRef.current?.value);
+      }}
+    >
+      <input type="search" name="search" id="search" ref={searchRef} />
+      <button type="submit">submit</button>
+    </form>
+  );
+}
+
+function Sort({
+  sort,
+  setSort,
+}: {
+  sort: string;
+  setSort: (type: string) => void;
+}) {
+  return (
+    <select
+      name="sort"
+      id="sort"
+      defaultValue={sort}
+      onChange={(e) => {
+        setSort(e.currentTarget.value);
+      }}
+    >
+      <option value="upvotes">upvotes</option>
+      <option value="downvotes">downvotes</option>
+      <option value="votes">votes</option>
+      <option value="positivity">positivity</option>
+      <option value="negativity">negativity</option>
+      <option value="last-added">last added</option>
+      <option value="last-voted">last voted</option>
+    </select>
+  );
+}
+
+function Pagination({
+  page,
+  setPrevPage,
+  setNextPage,
+}: {
+  page: number;
+  setPrevPage: () => void;
+  setNextPage: () => void;
+}) {
+  return (
+    <>
+      <button onClick={setPrevPage} disabled={page <= 1}>
+        prev
+      </button>
+      {/* TODO: max page */}
+      <button onClick={setNextPage}>next</button>
+    </>
+  );
+}
+
+function usePlaces({
+  sort,
+  page,
+  search,
+}: {
+  sort: string;
+  page: number;
+  search: string;
+}) {
+  const query = useQuery<Place[]>({
+    queryKey: ["places", sort, page, search],
+    queryFn: () =>
+      queryApi(`/places?sort=${sort}&page=${page}&search=${search}`, {
+        credentials: "include",
+      }),
+  });
+
+  return query;
 }
