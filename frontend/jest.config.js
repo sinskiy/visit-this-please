543ca7f5bc062347175e7fde89bc185ab087ee38
fluqont <fluqont@gmail.com>
2025-01-07 20:32:07 +0300

    feat(frontend): set up testing react components

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,3 +1,9 @@
+/** @type {import('jest').Config} */
 export default {
   modulePathIgnorePatterns: ["<rootDir>/e2e"],
+  moduleNameMapper: {
+    "\\.svg$": "<rootDir>/test/fileMock.ts",
+  },
+  setupFilesAfterEnv: ["<rootDir>/test/setup.tsx"],
+  testEnvironment: "jsdom",
 };
